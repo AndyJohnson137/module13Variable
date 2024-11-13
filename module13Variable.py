41d1045a511593a979bfeac1c7846e8272ad86e4
AndyJohnson137 <andyjohnson137@gmail.com>
2024-11-13 22:26:30 +0300

    Add files via upload
    "C:\Users\andyj\PycharmProjects\My first progect\.venv\Scripts\python.exe" "C:\Users\andyj\PycharmProjects\My first progect\module13Variable.py" 
    Курс: Python, всего задач:12, затрачено часов:1.5, среднее время выполнения 0.125 часа.
    
    Process finished with exit code 0

---
+++ module13Variable.py
@@ -0,0 +1,7 @@
+completed_hw = 12
+hours_spent = 1.5
+course_name = "Python,"
+time_for_one_task = (hours_spent / completed_hw)
+print(
+    f"Курс: {course_name} всего задач:{str(completed_hw) + ','} "
+    f"затрачено часов:{str(hours_spent) + ','} среднее время выполнения {str(time_for_one_task)} часа.")
